ba4dfd97967ffc47b88d02b6c675f9452df051f9
soundar <asoundarpandiyan@gmail.com>
2024-02-10 19:41:17 +1300

    Add project files.

---
+++ Bulky.Model/Category.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Bulky.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [DisplayName("Category Name")]
+        [StringLength(30, ErrorMessage ="String length should be maximum 30chars")]
+        public string Name { get; set; }
+
+        [DisplayName("Category Description")]
+        public string Description { get; set; }
+
+        [DisplayName("Display Order")]
+        [Range(1,100,ErrorMessage="Range should be between 1 and 100")]
+        public int DisplayOrder { get; set; } 
+
+    }
+}
---
+++ Bulky/Controllers/CategoryController.cs
@@ -0,0 +1,113 @@
+﻿using Bulky.DataAccess.Data;
+using Bulky.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bulky.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> categories = _db.Categories.ToList();
+            return View(categories);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            //custom validation
+            if (category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("DisplayOrder", "Display Order should not same as name");
+            }
+
+            if (category.Name is not null && category.Name.ToLower().Contains("test"))
+            {
+                ModelState.AddModelError("", "Category shouldn't have test content in the name");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(category);
+                _db.SaveChanges();
+                TempData["success"] = "Category created Successfully";
+                return RedirectToAction("Index", "Category");
+            }
+
+            return View();
+        }
+
+        public IActionResult Edit(int id)
+        {
+            Category? category = _db.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+
+            if (category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("DisplayOrder", "Display Order should not same as name");
+            }
+
+            if (category.Name is not null && category.Name.ToLower().Contains("test"))
+            {
+                ModelState.AddModelError("", "Category shouldn't have test content in the name");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(category);
+                _db.SaveChanges();
+                TempData["success"] = "Category updated Successfully";
+                return RedirectToAction("Index", "Category");
+            }
+
+            return View();
+        }
+
+        public IActionResult Delete(int id)
+        {
+            Category? category = _db.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int id)
+        {
+            Category? category = _db.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            _db.Categories.Remove(category);
+            _db.SaveChanges();
+            TempData["success"] = "Category deleted Successfully";
+            return RedirectToAction("Index", "Category");
+        }
+    }
+}
